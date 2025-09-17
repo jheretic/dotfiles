051af18c54ecaca90c96e3f6a3bfb884d850f9e7
Josh King <josh@throneless.tech>
2025-09-17 14:05:32 -0400

    Fix loop in hx alias

--- .config/fish/functions/hx.fish
+++ .config/fish/functions/hx.fish
@@ -1,9 +1,9 @@
 function hx --wraps=helix --description 'alias hx helix'
     if command -q helix
         helix $argv
-    else if command -q /usr/libexec/hx # For Fedora toolbox
+    else if command -q /usr/libexec/hx # For old Fedora toolbox
         HELIX_RUNTIME=/usr/share/helix/runtime /usr/libexec/hx $argv
-    else
-        hx $argv
+    else if command -q /usr/bin/hx # For new Fedora toolbox
+        /usr/bin/hx $argv
     end
 end
